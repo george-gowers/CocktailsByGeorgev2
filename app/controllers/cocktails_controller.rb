7caea8565b4046960010f03aa2c29a61a6c4f92d
George Gowers <georgegowers@gmail.com>
2022-03-31 13:38:10 +0200

    integrated twilio whatsapp API

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,19 +1,28 @@
-class CocktailController < ApplicationController
+class CocktailsController < ApplicationController
 
-  # before_action :create, only: [:show]
+  before_action :api, only: [:index]
 
   def index
+    account_sid = 'ACdda5d58aa60f5a284b47286328bfe0df'
+    auth_token = '17b42ae9fe5359a0cdd9070033dd48c5'
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    message = @client.messages.create(
+                             body:
+                             "Ingredients: #{@ingredients}
+                              Description: #{@description}",
+                             from: 'whatsapp:+14155238886',
+                             to: 'whatsapp:+31640110324'
+                           )
+
+  end
+
+  private
+
+  def api
     name = params[:name]
     response = Cocktail.api(name)
     @description = response[0]
     @ingredients = response[1]
   end
-
-  # def create
-  #   name = 'tonic'
-  #   response = Cocktail.api(name)
-  #   @description = response[0]
-  #   @ingredients = response[1]
-  # end
 
 end
